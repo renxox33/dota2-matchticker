ce3c293006a39964c7a61ac14adab5b1313b2a99
raj <raj@localhost.localdomain>
2020-09-07 13:50:07 +0530

    Project setup

---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+
+export default class HomeComponent extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Home Component using class component</h1>
+            </div>
+        )
+    }
+
+}
